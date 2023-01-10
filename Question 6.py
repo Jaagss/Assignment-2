285f1b3ab2f70098d93d1f7c06338fa2f2465959
Jaagss <jagadaleaditya057@gmail.com>
2023-01-10 19:08:22 +0530

    change

--- Question 6.py
+++ Question 6.py
@@ -2,14 +2,13 @@ wts = [(10, 5), (20, 5), (100, 15), (40, 10), (30,10), (10,5), (100,40), (40,10)
 f=open("Question 6.txt","r")
 a=f.read()
 l=[]
+s=[]
 for i in a:
     s=a.split("\n")
 for i in s:
     i=i.split(",")
     l.append(i)
-    # i=i[1::]
-    # for j in i:
-    #     j="".join(j.split())
-    #     j=int(j)
-    #     l.append(j)
-print(l)
+for i in l:
+    for j in range(len(i)):
+        i[j]=int(i[j])
+total_marks=0
