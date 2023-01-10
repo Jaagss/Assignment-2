71a002303c9e4694fa4f21583ad56297c027ea2b
Jaagss <jagadaleaditya057@gmail.com>
2023-01-10 18:48:41 +0530

    change

--- Question 6.py
+++ Question 6.py
@@ -1,4 +1,15 @@
 wts = [(10, 5), (20, 5), (100, 15), (40, 10), (30,10), (10,5), (100,40), (40,10)]
 f=open("Question 6.txt","r")
-a=f.readlines()
-print(a)
+a=f.read()
+l=[]
+for i in a:
+    s=a.split("\n")
+for i in s:
+    i=i.split(",")
+    l.append(i)
+    # i=i[1::]
+    # for j in i:
+    #     j="".join(j.split())
+    #     j=int(j)
+    #     l.append(j)
+print(l)
