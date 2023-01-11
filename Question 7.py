0b0eae5c658a0ed33120b79dd58282c98b9b89c5
Jaagss <jagadaleaditya057@gmail.com>
2023-01-11 12:25:07 +0530

    change

---
+++ Question 7.py
@@ -0,0 +1,6 @@
+address={}
+def insert():
+    name=input()
+while True:
+    choice=int(input("1 : inserting new entry: \n2 : delete an entry: \n3 : find all entries from name \n4 : Find the entry with phone number \n5 : Exit\n\nPlease enter your choice: "))
+    if choice==1:
