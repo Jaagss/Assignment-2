33e099f7d41609b500b0dea5bb8eb5be7150dd21
Jaagss <jagadaleaditya057@gmail.com>
2023-01-11 16:25:23 +0530

    Change 5 to while loop for inputs

--- Question 5.py
+++ Question 5.py
@@ -4,11 +4,13 @@ for i in range(n):
     l1=list(map(int,input().split()))
     l1+=[1]
     l.append(l1)
-cx=int(input())
-cy=int(input())
+cx=int(input("Enter cx: "))
+cy=int(input("Enter cy: "))
 matrix=[[cx,0,0],[0,cy,0],[0,0,1]]
 
 result = [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*matrix)] for X_row in l]
 
 for r in result:
-   print(r)
+    for i in range(len(r)-1):
+        print(r[i],end=",")
+        print()
