63cd41623c4c0708e6b32dd03d88940d1f38fef7
Jaagss <jagadaleaditya057@gmail.com>
2023-01-15 17:28:02 +0530

    need to do same names, partial name return entries

--- Question 7.py
+++ Question 7.py
@@ -1,19 +1,19 @@
 import ast
 phone_book={}
+
+
 def read():
-    f=open("addrbook.txt","r")
+    f=open("addrbook.txt","r+")
     phone_book=ast.literal_eval(f.read())
     f.close()
     return phone_book
 
-def write():
+def write(phone_book):
     f=open("addrbook.txt","w")
-    s=str(phone_book)
-    f.write(s)
+    f.write(str(phone_book))
     f.close()
 
-def insert():
-    phone_book=read()
+def insert(phone_book):
     name=input("Enter name: ")
     dict={}
     phone_book[name]=dict
@@ -25,39 +25,49 @@ def insert():
     dict["email"]=email
     return phone_book
 
-def delete():
-    phone_book=read()
+def delete(phone_book):
     del_entry=input("Enter entry to be deleted: ")
     del phone_book[del_entry]
     return phone_book
 
-def find_name():
-    phone_book=read()
+def find_name(phone_book):
     name_find=input("Enter name to search: ")
     return phone_book[name_find]
 
-def find_phone():
-    phone_book=read()
+def find_phone(phone_book):
     phone=int(input("Enter phone number to search: "))
-    res=[val[phone] for key,val in phone_book.items() if phone in val]
-    return res
+    for keys,values in phone_book.items():
+        for i,j in values.items():
+            if j == phone:
+                return keys,phone_book[keys]
 
+f = open("addrbook.txt")
+if(len(f.read().splitlines())==0):
+    phone_book = {}
+    pass
+else:
+    phone_book = read()
+f.close()
 while True:
     choice=int(input("1 : Inserting new entry: \n2 : Deleting an entry: \n3 : Find all entries from name \n4 : Find the entry with phone number \n5 : Exit\n\nPlease enter your choice: "))
     if choice==1:
-        new_phone_book=insert()
+        new_phone_book=insert(phone_book)
         phone_book={**phone_book,**new_phone_book}
+        print(phone_book)
         print()
     elif choice==2:
-        new_phone_book=delete()
+        new_phone_book=delete(phone_book)
         phone_book={**phone_book,**new_phone_book}
         print()
     elif choice==3:
-        print(find_name())
+        print()
+        print(find_name(phone_book))
         print()
     elif choice==4:
-        print(find_phone())
+        print()
+        key,value = find_phone(phone_book)
+        print(key,value)
         print()
     elif choice==5:
-        write()
+        write(phone_book)
         break
